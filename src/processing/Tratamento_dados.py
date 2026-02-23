ffc5f47feb0640850ae0179f15b2e5589b8a6f70
LauroGobatto <lauroz1232@gmail.com>
2026-02-23 15:42:37 -0300

    Ajustes de erros após teste de workflow

--- src/processing/Tratamento_dados.py
+++ src/processing/Tratamento_dados.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 
-df_iptu_residencial = pd.read_csv('data/raw/IPTU_RESIDENCIAL.csv', encoding='utf-8-sig')
+df_iptu_residencial = pd.read_csv('data/raw/IPTU_RESIDENCIAL.csv', encoding='utf-8-sig', sep=',')
 mes_ano = datetime.now().strftime('%Y_%m')
 arquivos = ['TOTAL', mes_ano ]
 
@@ -17,7 +17,7 @@ for i in arquivos:
     else:
         webscrappingfinal = pd.read_csv(f'data/processed/monthly/PREÇO_POR_BAIRRO_{i}.csv')
     
-    df_iptu_residencial.columns = [c.replace('\ufeff', '').strip() for c in df_iptu_residencial.columns]
+    df_iptu_residencial.columns = df_iptu_residencial.columns.str.strip().str.replace('\ufeff', '')
     df_iptu_residencial['nome'] = df_iptu_residencial['nome'].str.strip()
     df_iptu_residencial = df_iptu_residencial.rename(columns= {
         'nome': 'BAIRRO',
@@ -33,8 +33,8 @@ for i in arquivos:
 
 
 
-    df_iptu_territorial = pd.read_csv('data/raw/IPTU_TERRITORIAL.csv', encoding = 'utf-8-sig')
-    df_iptu_territorial.columns = [c.replace('\ufeff', '').strip() for c in df_iptu_territorial.columns]
+    df_iptu_territorial = pd.read_csv('data/raw/IPTU_TERRITORIAL.csv', encoding = 'utf-8-sig', sep = ',')
+    df_iptu_territorial.columns = df_iptu_territorial.columns.str.strip().str.replace('\ufeff', '')
     df_iptu_territorial['nome'] = df_iptu_territorial['nome'].str.strip()
     df_iptu_territorial = df_iptu_territorial.rename(columns= {
         'nome': 'BAIRRO',
@@ -57,7 +57,7 @@ for i in arquivos:
     df_renda = pd.read_csv('data/raw/RENDA_POR_BAIRRO.csv')
     df_renda.rename(columns={df_renda.columns[0]: 'BAIRRO'}, inplace=True)
     df_renda.rename(columns={df_renda.columns[11]: 'RENDA MENSAL'}, inplace=True)
-    df_renda_bairro = df_renda[['BAIRRO','RENDA MENSAL']]
+    df_renda_bairro = df_renda[['BAIRRO','RENDA MENSAL']].copy()
     df_renda_bairro['RENDA MENSAL'] = (df_renda_bairro['RENDA MENSAL'] * 1212 * 1.165).round(2)
     df_final = pd.merge(df_merge_territorial, df_renda_bairro, on= "BAIRRO", how='inner')
     df_final['ÍNDICE DE ACESSIBILIDADE'] = ((df_final['PREÇO POR METRO'] * 50) / df_final['RENDA MENSAL'])
