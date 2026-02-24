4ebe9394d6b47f7a537015b82465df9b0e321c1a
LauroGobatto <lauroz1232@gmail.com>
2026-02-24 15:15:59 -0300

    Aperfeiçoamento do modelo preditivo

--- output/scripts_graficos/GRÁFICO_DE_CALOR.py
+++ output/scripts_graficos/GRÁFICO_DE_CALOR.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time
 import os
 
-df_riscos = pd.read_csv('data/processed/RISCO DE GENTRIFICAÇÃO.csv')
+df_riscos = pd.read_csv('data/processed/BAIRROS_RENDA_TOTAL.csv')
 geolocator = Nominatim(user_agent="meu_projeto_gentrificacao")
 
 coluna_latitude = []
@@ -13,10 +13,10 @@ for bairro in df_riscos['BAIRRO']:
     local = geolocator.geocode(f"{bairro}, Rio de Janeiro")
     coluna_latitude.append(local.latitude)
     coluna_longitude.append(local.longitude)
-    time.sleep(1)
+    time.sleep(1.5)
 
 df_riscos['LATITUDE'] = coluna_latitude
 df_riscos['LONGITUDE'] = coluna_longitude
 
-caminho_csv = os.path.join('data', 'raw', 'COORDENADAS.csv')
+caminho_csv = os.path.join('data', 'processed', 'COORDENADAS.csv')
 df_riscos.to_csv(caminho_csv, index = False)
--- src/models/MODELO.py
+++ src/models/MODELO.py
@@ -4,10 +4,15 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import VotingClassifier, GradientBoostingClassifier
 from sklearn.metrics import classification_report
+from sklearn.neighbors import KNeighborsRegressor
 from imblearn.over_sampling import SMOTE
+from imblearn.combine import SMOTETomek
+from scipy.stats import zscore
 import os
+import shap
 
-df = pd.read_csv('data/processed/BAIRROS_RENDA_TOTAL.csv')
+
+df = pd.read_csv('data/processed/COORDENADAS.csv')
 
 media_renda = df['RENDA MENSAL'].mean()
 media_preco = df['PREÇO POR METRO'].mean()
@@ -19,43 +24,51 @@ renda_maior85 = df['RENDA MENSAL'].quantile(0.85)
 area_menor50 = df['ÁREA TERRITORIAL DISPONÍVEL'].quantile(0.50)
 
 def rotular_risco(row):
-    if (row['ÍNDICE DE ACESSIBILIDADE'] > 0.9) and (row['SCORE FINAL'] > mediana_score) and (row['ÍNDICE DE PRESSÃO'] > mediana_pressao):
+    if (row['ÍNDICE DE ACESSIBILIDADE'] > 0.8) and (row['SCORE FINAL'] > mediana_score) and (row['ÍNDICE DE PRESSÃO'] > mediana_pressao):
         return 2  # Risco Alto
-    elif (row['RENDA MENSAL'] < media_renda) and (row['PREÇO POR METRO'] < media_preco) and (row['ÁREA TERRITORIAL DISPONÍVEL'] > area_maior80):
-        return 1  # Risco Médio
+    elif (row['RENDA MENSAL'] < media_renda) and (row['ÍNDICE DE PRESSÃO'] > mediana_pressao) and (row['ÁREA TERRITORIAL DISPONÍVEL'] > area_maior80):
+        return 1
+    elif (row['ÍNDICE DE ACESSIBILIDADE'] > 0.5) and (row['RENDA MENSAL'] < media_renda) and (row['ÁREA TERRITORIAL DISPONÍVEL'] > area_maior80):
+        return 1
     else:
         return 0  # Risco Baixo
 
 
-df['PROXIMIDADE_RISCO'] = (
-    (df['ÍNDICE DE PRESSÃO'] / mediana_pressao) + 
-    (df['ÍNDICE DE ACESSIBILIDADE'] / 0.9) + 
-    (df['SCORE FINAL'] / mediana_score)
-) / 3
-df['PRESSAO_VALORIZACAO'] = df['PREÇO POR METRO'] / (df['RENDA MENSAL'] + 1)
-
+df['POTENCIAL_TRANSFORMACAO'] = (zscore(df['ÍNDICE DE PRESSÃO']) + 
+                                zscore(df['ÍNDICE DE ACESSIBILIDADE'])) / 2
 
 df['RISCO_TARGET'] = df.apply(rotular_risco, axis=1)
 
-
-features = df[['PREÇO POR METRO', 'ÍNDICE DE PRESSÃO', 'ÁREA TERRITORIAL DISPONÍVEL', 'RENDA MENSAL', 'ÍNDICE DE ACESSIBILIDADE', 'SCORE FINAL', 'PROXIMIDADE_RISCO', 'PRESSAO_VALORIZACAO']]
-X = features
 y = df['RISCO_TARGET']
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
+coords = df[['LATITUDE', 'LONGITUDE']]
+features_base = ['PREÇO POR METRO', 'ÍNDICE DE PRESSÃO', 'ÁREA TERRITORIAL DISPONÍVEL', 'RENDA MENSAL', 'ÍNDICE DE ACESSIBILIDADE', 'SCORE FINAL', 'POTENCIAL_TRANSFORMACAO']
+X = df[features_base]
+X_train, X_test, y_train, y_test, coords_train, coords_test = train_test_split(
+    X, y, coords, test_size=0.2, random_state=42, stratify=y
 )
-smote = SMOTE(random_state=42, k_neighbors=1)
+
+knn = KNeighborsRegressor(n_neighbors = 3)
+knn.fit(coords_train, y_train)
+
+X_train = X_train.copy()
+X_test = X_test.copy()
+
+X_train['RISCO_VIZINHANCA'] = knn.predict(coords_train)
+X_test['RISCO_VIZINHANCA'] = knn.predict(coords_test)
+df['RISCO_VIZINHANCA'] = knn.predict(df[['LATITUDE', 'LONGITUDE']])
+
+smote_custom = SMOTE(random_state=42, k_neighbors=3) 
+smote = SMOTETomek(random_state=42, smote=smote_custom)
+
 X_train_res, y_train_res = smote.fit_resample(X_train, y_train)
 
-
+rf_base = RandomForestClassifier(random_state=42, class_weight='balanced')
 param_grid = {
     'n_estimators': [100, 200, 300],
     'max_depth': [None, 10, 20],
     'min_samples_split': [2, 5, 10]
 
 }
-rf_base = RandomForestClassifier(random_state=42, class_weight='balanced')
-
 grid_search = GridSearchCV(rf_base , param_grid, cv=5)
 grid_search.fit(X_train_res, y_train_res)
 melhor_rf = grid_search.best_estimator_
@@ -73,14 +86,23 @@ y_pred = modelo.predict(X_test)
 print("\n--- RELATÓRIO DE DESEMPENHO ---")
 print(classification_report(y_test, y_pred, zero_division=0))
 
+features_predicao = [
+    'PREÇO POR METRO', 
+    'ÍNDICE DE PRESSÃO', 
+    'ÁREA TERRITORIAL DISPONÍVEL', 
+    'RENDA MENSAL', 
+    'ÍNDICE DE ACESSIBILIDADE', 
+    'SCORE FINAL',
+    'POTENCIAL_TRANSFORMACAO',
+    'RISCO_VIZINHANCA'
+]
+probabilidades = modelo.predict_proba(df[features_predicao])
 
-probabilidades = modelo.predict_proba(features)
+df['RISCO ALTO'] = ((probabilidades[:, 1] * 50) + (probabilidades[:, 2] * 100)).round(2)
 
-df['RISCO ALTO'] = ((probabilidades[:, 1] * 40) + (probabilidades[:, 2] * 110)).round(2)
-
-df = df.drop(columns= ['PROXIMIDADE_RISCO'])
-df = df.drop(columns= ['PRESSAO_VALORIZACAO'])
+df = df.drop(columns= ['POTENCIAL_TRANSFORMACAO'])
 df = df.drop(columns= ['RISCO_TARGET'])
+df = df.drop(columns= ['RISCO_VIZINHANCA'])
 
 df = df.sort_values(by='RISCO ALTO', ascending=False).reset_index(drop = True)
 print("\n--- RADAR DE GENTRIFICAÇÃO (RISCO 2) ---")
@@ -91,3 +113,23 @@ raiz = os.path.dirname(os.path.dirname(diretorio_atual))
 
 caminho_csv = os.path.join(raiz, 'data', 'processed', 'RISCO DE GENTRIFICAÇÃO.csv')
 df.to_csv(caminho_csv, index= False)
+
+background = shap.sample(X_train, 50) 
+
+# 3. O PULO DO GATO: Usar o predict_proba do MODELO PRINCIPAL (que está treinado)
+# Usamos o Lambda para o SHAP não se perder nas classes do sklearn
+model_func = lambda x: modelo.predict_proba(x)
+
+# 4. Criar o explicador
+explainer = shap.KernelExplainer(model_func, background)
+
+# 5. Testar com poucas amostras (KernelExplainer é lento)
+X_test_sample = X_test.sample(10, random_state=42)
+shap_values = explainer.shap_values(X_test_sample)
+
+# 6. Plotar (Classe 2 = Geralmente 'Risco Alto' em 3 classes)
+# Verifique se shap_values é uma lista (comum no KernelExplainer)
+if isinstance(shap_values, list):
+    shap.summary_plot(shap_values[2], X_test_sample)
+else:
+    shap.summary_plot(shap_values[:,:,2], X_test_sample)
